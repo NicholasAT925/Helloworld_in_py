2d7ee16ddf049bc49e2b950e1d4a94b47e1e21f3
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 13:41:31 +0800

    Update HelloWorld.py

--- In_py/HelloWorld.py
+++ In_py/HelloWorld.py
@@ -1,1 +1,13 @@
 print("\nHello World\n")
+
+word1 = "hello"
+word2 = "my"
+word3 = "bob"
+
+print(word1 + word2 + word3 + "\n")
+print(word1 , word2 , word3 , "\n") 
+
+age = 23
+
+print("I am", age, "\n")
+# print("I am" + age)// This produces an error as you cannot concatinate string and int together.
